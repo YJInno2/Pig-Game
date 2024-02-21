b11d7cb6cea02464b856a229bcdf45e66c4a5ac9
YJ32 <hetytaina@gmail.com>
2024-02-21 14:36:34 +0800

    Add switch player functionality

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
-const section0El = document.querySelector('.player--0');
-const section1El = document.querySelector('.player--1');
 
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
@@ -38,7 +38,14 @@ btnRoll.addEventListener('click', function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
+    // Reset active player's current score
+    [
+      currentScore,
+      document.getElementById(`current--${activePlayer}`).textContent,
+    ] = [0, 0];
     // switch to next player
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
